0463c4115ca48c97335869149b01fd34e8576223
Nikita <nikturchinovich@mail.ru>
2024-05-04 12:43:42 +0300

    lab09 is done

---
+++ lab09/CMS-2-2-lab09/Assets/Input_GetAxis.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_GetAxis : MonoBehaviour {
+	float _translateX, _translateZ;
+	float _rotateX, _rotateY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_translateX = Input.GetAxis("Horizontal");
+		_translateZ = Input.GetAxis("Vertical");
+
+		_rotateX = Input.GetAxis("Mouse X");
+		_rotateY = Input.GetAxis("Mouse Y");
+
+		_rotateY = Mathf.Clamp(_rotateY, 0, 90);
+
+		transform.Translate(_translateX, 0, _translateZ);
+		transform.Rotate(_rotateX, _rotateY, 0);
+	}
+}
